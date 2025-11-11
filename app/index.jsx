e3d899ac12657e2def57539fc10925ce678e13ed
MrDisgusted <202101534@student.polytechnic.bh>
2025-11-11 14:21:36 +0300

    added configurations

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import '../global.css';
+
+const index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})

b54de5815efa23601e721032a9c556e58756c36d
MrDisgusted <202101534@student.polytechnic.bh>
2025-11-12 21:21:22 +0300

    added icons

---
+++ constants/icons.ts
@@ -0,0 +1,13 @@
+import food from "../assets/icons/food.png";
+import menu from "../assets/icons/menu.png";
+import pills from "../assets/icons/pills.png";
+import run from "../assets/icons/run.png";
+import weights from "../assets/icons/weights.png";
+
+export const icons = {
+    food,
+    menu,
+    pills,
+    run,
+    weights
+}
